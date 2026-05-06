221f64984d11aa058100905bc039ea01f22b5c61
vinayak-salian <139992655+vinayak-salian@users.noreply.github.com>
2026-05-06 20:45:07 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ import requests
 import sqlite3
 
 # --- CONFIG ---
-API_BASE = "https://ntsc-curtis-placing-essential.trycloudflare.com"
+API_BASE = "https://roots-sentence-options-medication.trycloudflare.com"
 DB_PATH = "/home/vinayak/honeypot_project/nexus_security.db"
 
 st.set_page_config(page_title="Nexus Security Core", layout="wide")
